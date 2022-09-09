9e1fbeb35dfdb936361f13c4c9c05c9f1fb89eb2
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-08 22:03:11 -0400

    ad task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	system("cout 'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n'");
+	return (0);
+}
