65abd92f49f14d2618ab1b384c03ec67eb854dce
_Yuu <91846346+ahmedelia@users.noreply.github.com>
2022-09-09 20:37:05 -0400

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	char z = 'a';
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar("%d", x);
+		x++;
+	}
+
+	while (z <= 'f')
+	{
+		putchar(z);
+		z++;
+	}
+		putchar('\n');
+
+	return (0);
+}
