c03ced043557de717e24b536f74169e588044605
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-13 16:18:30 -0400

    hate betty again

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - check the code
+ * @c: char to be checked
+ * Return: 0 or 1 depend on code
+ */
+
+int _isalpha(int c)
+{
+	int is = isalpha(c);
+
+	if (is)
+		return (1);
+	return (0);
+}
