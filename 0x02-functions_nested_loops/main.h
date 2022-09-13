7318d61a7015098384b9fb779153134c4df61def
_Yuu <91846346+ahmedelia@users.noreply.github.com>
2022-09-13 15:08:34 -0400

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,9 @@
 /**
  * _putchar - prototype.
  * print_alphabet - print a..z
+ * print_alphabet_x10 - print a..z 10 times
  * Return: nothing.
  */
-
+void print_alphabet_x10(void);
 int _putchar(char c);
 void print_alphabet(void);
