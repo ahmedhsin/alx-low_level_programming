44db87cab25756e252e5344af1cb17081e036bdb
_Yuu <91846346+ahmedelia@users.noreply.github.com>
2022-09-13 17:45:38 -0400

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * print_last_digit - check the code
+ * @c: char to be checked
+ * Return: 0 or 1 depend on code
+ */
+
+int print_last_digit(int c);
+{
+
+	if (c < 0)
+		c = c * -1;
+  _putchar(c % 10 + '0');
+  return (c % 10);
+}
