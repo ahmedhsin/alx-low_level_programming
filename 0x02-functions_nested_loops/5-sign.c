46d5be64a610611bfd93fec1a5c1a34e60eacb18
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-13 16:49:36 -0400

    hate betty again

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code
+ * @n: char to be checked
+ * Return: 0 or 1 depend on code
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+		return (0);
+	else if (n > 0)
+		return (1);
+	else
+		return (-1);
+}
