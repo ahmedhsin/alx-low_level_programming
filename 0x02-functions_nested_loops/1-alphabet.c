1efddc699f73717387a056e0025db93ebd3b01ee
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-13 14:40:32 -0400

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * print_alphabet - print a..z
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+}
