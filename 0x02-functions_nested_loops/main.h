4debf71521e56c4e309d7a89beed56d328ae7e2b
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-13 16:03:45 -0400

    third task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /**
+ * _islower - check Lower Case Char
  * _putchar - prototype.
  * print_alphabet - print a..z
  * print_alphabet_x10 - print a..z 10 times
@@ -9,3 +10,4 @@
 void print_alphabet_x10(void);
 int _putchar(char c);
 void print_alphabet(void);
+int _islower(int c);
