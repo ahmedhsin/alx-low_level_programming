8fb3bafbb698681248d4edf99a85aadc2b13e341
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-13 22:17:03 -0400

    lol

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: int
+ */
+
+int main(void)
+{
+	int i = 47;
+	int first = 1;
+	int second = 2;
+	int next = 0;
+
+	printf("%d, ", first);
+	printf("%d, ", second);
+	while (i--)
+	{
+		next = first + second;
+		printf("%d, ", next);
+		first = second;
+		second = next;
+	}
+	printf("%d\n", second + first);
+}
