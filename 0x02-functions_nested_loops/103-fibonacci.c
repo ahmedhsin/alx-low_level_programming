50f633f7905c22bd1624b7404cf8eae8801b8813
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-13 22:40:42 -0400

    lol

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: int
+ */
+
+int main(void)
+{
+	long int first = 1;
+	long int second = 2;
+	long int next = 0;
+	long int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = first + second;
+		first = second;
+		second = next;
+		if (next % 2 == 0)
+			sum = sum + next;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
