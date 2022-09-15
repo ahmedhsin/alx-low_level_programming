90721e7c3e883f749c839e3a4b5c2ad467877c4b
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-15 19:43:32 -0400

    oook

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < "10"; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
