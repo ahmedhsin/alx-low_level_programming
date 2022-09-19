c39a087b2ebc673f0425546dd574aece0b1fb62a
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-19 19:56:12 -0400

    third

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,14 @@ void puts_half(char *str)
 {
 	int i;
 	int len = 0;
+	int k = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
+	if (len % 2 == 0)
+		k = len / 2;
+	else
+		k = len / 2 + 1;
 	for (i = len / 2; i < len; i++)
 	{
 		_putchar(str[i]);
