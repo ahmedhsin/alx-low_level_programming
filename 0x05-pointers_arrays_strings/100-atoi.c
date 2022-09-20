2b413f15f99d94cd85aafc785a2bc3e53ce281c2
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-19 21:35:41 -0400

    fiiiii

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,45 +10,45 @@
 
 int _atoi(char *s)
 {
-    unsigned int re = 0;
-    int i;
-    int cd = 1;
-    int start = 0;
-    int pp = 0;
-    int ss = 0;
-    int sp;
-    int numz = 0;
+	unsigned int re = 0;
+	int i, cd = 1, start = 0, pp = 0, ss = 0, sp, numz = 0;
 
-    for (i = 0;s[i] != '\0'; i++)
-    {
-        if(s[i] >= '0' && s[i] <= '9') {
-            if(!ss){
-                ss = 1;
-                start = i;
-            }
-        }
-        if(ss){
-            if(s[i] >= '0' && s[i] <= '9') pp++;
-            else
-                break;
-        }
-    }
-    sp = pp;
-    sp -= 1;
-    while(sp--){
-        cd *= 10;
-    }
-    for (i = 0;i < start; i++)
-    {
-        if (s[i] == '-') numz++;
-    }
-    for (i = start; i < start + pp; i++)
-    {
-        re = re  + cd * (s[i]-'0');
-        cd /= 10;
-    }
-    if (numz % 2 != 0)
-        return (re * -1);
-    else
-        return (re);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (!ss)
+			{
+				ss = 1;
+				start = i;
+			}
+		}
+		if (ss)
+		{
+			if (s[i] >= '0' && s[i] <= '9')
+				pp++;
+			else
+				break;
+		}
+	}
+	sp = pp;
+	sp -= 1;
+	while (sp--)
+	{
+		cd *= 10;
+	}
+	for (i = 0; i < start; i++)
+	{
+		if (s[i] == '-')
+			numz++;
+	}
+	for (i = start; i < start + pp; i++)
+	{
+		re = re  + cd * (s[i] - '0');
+		cd /= 10;
+	}
+	if (numz % 2 != 0)
+		return (re * -1);
+	else
+		return (re);
 }
