74afbf251efdd007d61f478c3b5aacc726f5f179
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-26 21:50:09 -0400

    tt

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * print_diagsums - check the code
+ * @a: blablalba
+ * @size : bbbl
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0, sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += (a + size * i)[i];
+	}
+	printf("%d ,", sum);
+	sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += (a + size * i)[size-1-i];
+	}
+	printf("%d\n", sum);
+}
