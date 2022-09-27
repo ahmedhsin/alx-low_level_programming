53b73c8fdb8566da929bf9f4af18eeb452f68e61
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-26 20:05:28 -0400

    tt

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * *_memset - check the code
+ * @s: blablalba
+ * @b: blablalb
+ * @n: bbbb
+ * Return: void
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s+i) = b;
+	}
+	return (s);
+}
