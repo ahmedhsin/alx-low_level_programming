26f94f8b075432d95b603315c892f58af49f8a96
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-26 22:02:23 -0400

    ff

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * set_string - check the code
+ * @s: blablalba
+ * @to:sdadsa
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
