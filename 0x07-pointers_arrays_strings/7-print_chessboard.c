9543d5d9fb19ffd92f9a7caedbb5829801ed1d4e
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-26 21:34:15 -0400

    tt

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * print_chessboard - check the code
+ * @a: blablalba
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
