8ac7b3f8cdfc878eb649d2f1a8c1f84188f4fffe
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-28 22:19:21 -0400

    sol

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - check the code
+ * @s: blablalba
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s+1);
+		_putchar(*s);
+
+	}
+}
