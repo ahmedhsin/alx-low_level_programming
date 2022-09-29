1a1bcea7f5a2ed93ad9ef5e066886adbcdb2dcbc
_Yuu <91846346+ahmedelia@users.noreply.github.com>
2022-09-28 22:15:19 -0400

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,14 +10,14 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (*s)
 	{
 		return;
 	}
 	else
 	{
-		_print_rev_recursion(++s);
-		_putchar(s[0]);
+		_print_rev_recursion(s+1);
+		_putchar(*s);
 
 	}
 }
