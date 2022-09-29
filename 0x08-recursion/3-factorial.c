e1820750e2fb0954e99263d70864e1c0f1318288
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-28 20:13:34 -0400

    test first task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * int factorial(int n); - check the code
+ * @n: blablalba
+ * Return: void
+ */
+
+int factorial(int n);
+{
+	if (n  < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
