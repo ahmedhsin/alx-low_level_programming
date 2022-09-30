b39a2f28d48064814b9c55404dcb716a52ae2ede
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2022-09-30 17:04:00 -0400

    s task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* @argc:blaaaaa
+* @argv :laaaa
+* Return: Always 0
+*/
+int main(int argc, char __attribute__((__unused__))  *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
