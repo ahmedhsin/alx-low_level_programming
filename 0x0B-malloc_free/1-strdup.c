05c6c4892dbfffa999c8e17cff3b73f6161be57f
Yuu <ahmed.elia2003@gmail.com>
2022-10-05 01:28:05 +0200

    task -1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,17 +15,17 @@ char *_strdup(char *str)
 	unsigned int size;
 	char *ptr;
 	unsigned int i;
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	size = strlen(str);
 	ptr = malloc(size + 1);
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < size; i++)
 		ptr[i] = str[i];
 	return (ptr);
