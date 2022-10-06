2bdc23adac1f9778eea0413ce5520377977f865d
Yuu <ahmed.elia2003@gmail.com>
2022-10-07 00:22:09 +0200

    inti-commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - check the code
+ * @b:blaaaaaaaaa
+ *
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		return (98);
+	else
+		return (ptr);
+}
