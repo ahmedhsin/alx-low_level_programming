53e9814d53618319d2c8d34c896caa8628f5276c
Yuu <ahmed.elia2003@gmail.com>
2022-10-07 01:01:34 +0200

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	else
-		return (ptr);
+	for (i = 0; i < nmemb; i = i + size)
+	{
+		*(ptr + i) = 0;
+	}
+	return (ptr);
 }
