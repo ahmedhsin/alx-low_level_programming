e5621dbeb8e46dd66e3f88d513ae9caa0ec783d2
Yuu <ahmed.elia2003@gmail.com>
2022-10-10 22:12:21 +0200

    tcommit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * *new_dog - check the code
+ * @name: blllll
+ * @age: blllll
+ * @owner: blllll
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(dog_t);
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return (ptr);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,20 @@
 #define DOG_H
 #include <stdio.h>
 /**
+ * dog_t - typedef for dog;
+ */
+typedef struct dog dog_t;
+/**
  * struct dog - dog structure
  * @name: f
  * @owner: f
  * @age: f;
  *
  */
-
 struct dog
 {
 	char *name, *owner;
 	float age;
 };
+
 #endif
