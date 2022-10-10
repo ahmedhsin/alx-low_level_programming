7ab14d82fd453aeb345e1da34ef1a34525b5d441
Yuu <ahmed.elia2003@gmail.com>
2022-10-10 22:17:55 +0200

    tcommit

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <dog.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
