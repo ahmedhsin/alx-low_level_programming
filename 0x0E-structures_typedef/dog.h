fad742c9e19f9c7f229edda9f471e4dec59e537e
Yuu <ahmed.elia2003@gmail.com>
2022-10-10 21:39:47 +0200

    fcommit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - dog structure
+ * @name: f
+ * @owner: f
+ * @age: f;
+ *
+ */
+
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
+#endif
