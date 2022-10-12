6aab70f08ec9651e349131d0237143d3243c2c40
Yuu <ahmed.elia2003@gmail.com>
2022-10-13 01:31:14 +0200

    stask

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
-	if (action == NULL || array == NULL || size == 0)
+	if (action == NULL || array == NULL || size <= 0)
 		return;
 	for (i = 0; i < size; i++)
 	{
