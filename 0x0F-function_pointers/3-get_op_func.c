e0dd618d750660cf4a9f3baf619c2a3dcbf418cc
Yuu <ahmed.elia2003@gmail.com>
2022-10-13 04:05:43 +0200

    llll

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+ * get_op_func - check the code
+ * @s:booo
+ *
+ * Return: Always 0.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i < 6)
+	{
+		if (strcmp(s,ops[i].op))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - check the code
+ * @argc: lool
+ * @argv: pointer
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int ans;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	f = get_op_func(argv[2]);
+        if (f == NULL)
+        {
+                printf("Error\n");
+                return (99);
+        }
+        ans = f(atoi(argv[1]), atoi(argv[3]));
+        if (ans == INT_MAX)
+        {
+                printf("Error\n");
+                return (100);
+        }
+        printf("%d\n", ans);
+        return (0);
+}
