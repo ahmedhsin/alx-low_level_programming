99f66a3374682ea5614e218d4cc4e201e6d537fe
Yuu <ahmed.elia2003@gmail.com>
2022-10-14 03:11:42 +0200

    stask

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - check the code
+ * @n:ll
+ * @separator:ll
+ * @...: var
+ *
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	if (n == 0)
+		return (0);
+	if (separator == NULL)
+		separator = "";
+	va_start(arg, n);
+	printf("%d", va_arg(arg, int));
+	for (i = 0; i < n - 1; i++)
+		printf("%s%d", separator, va_arg(arg, int));
+	return (0);
+}
