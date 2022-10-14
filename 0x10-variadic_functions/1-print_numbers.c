164dcc7ba90f1b3bc7eca84721fb787087a3dc37
Yuu <ahmed.elia2003@gmail.com>
2022-10-14 03:37:11 +0200

    stask

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,13 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list arg;
 
-	if (n == 0)
-		return;
 	va_start(arg, n);
 	printf("%d", va_arg(arg, int));
 	for (i = 0; i < n - 1; i++)
+	{
 		if (separator != NULL)
 			printf("%s%d", separator, va_arg(arg, int));
 		else
 			printf("%d", va_arg(arg, int));
+	}
+	printf("\n");
 }
