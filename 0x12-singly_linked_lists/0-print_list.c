6e105c96c28edf8ac7f59a85cdd7acdf09e36e0f
Yuu <ahmed.elia2003@gmail.com>
2022-10-21 01:50:47 +0200

    init-commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - print func on linkedlist
+ * @h: pointer to the head of list
+ * Return: numbers of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	list_t *current = h;
+	size_t numofnodes = 0;
+
+	while (current)
+	{
+		if (!current->str)
+		{
+			current->str = "(nil)";
+			current->len = 0;
+		}
+		printf("[%d] %s\n", current->len, current->str);
+		numofnodes++;
+		current = current->next;
+	}
+	return (numofnodes);
+}
