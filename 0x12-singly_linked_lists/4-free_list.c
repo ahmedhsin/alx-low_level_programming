fcc329afaac70cf57494efeef74805b8c5d64cad
Yuu <ahmed.elia2003@gmail.com>
2022-10-21 03:21:34 +0200

    #6 -commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - print func on linkedlist
+ * @head: pointer to the head of list
+ * Return: numbers of nodes
+*/
+void free_list(list_t *head)
+{
+	list_t *next = head;
+
+	while (head)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
