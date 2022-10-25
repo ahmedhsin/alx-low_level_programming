921c556c38d323fec4dfde4fe51b2bb1f33d40a7
Yuu <ahmed.elia2003@gmail.com>
2022-10-25 05:19:21 +0200

    #7 task -#2-fix commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,14 +7,13 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current = head;
-	int i = 0;
+	unsigned int i = 0;
 
-	while (current)
+	while (head)
 	{
 		if (i == index)
-			return (current);
-		current = current->next;
+			return (head);
+		head = head->next;
 		i++;
 	}
 	return (NULL);
