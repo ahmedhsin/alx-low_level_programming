461152462a92bc66e31a377c159a0d032d9e65e1
Yuu <ahmed.elia2003@gmail.com>
2022-10-25 07:31:57 +0200

    #10 task -#2-fix commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,6 +10,15 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *tmp, *curr = *head;
 
+	if (!head || !(*head))
+		return (-1);
+	if (index == 0)
+	{
+		tmp = curr->next;
+		free(curr);
+		*head = tmp;
+		return(1);
+	}
 	while (curr)
 	{
 		if (i == index - 1)
