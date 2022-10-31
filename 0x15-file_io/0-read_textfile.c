49c60eda53c630a81ee774e897a5eb381f1c9c4c
Yuu <ahmed.elia2003@gmail.com>
2022-10-31 14:05:31 +0200

    #1 task - fix #2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,9 +8,12 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = open(filename, O_RDONLY);
-	int *buf = malloc(letters);
+	int *buf;
 	int total = 0;
 
+	if (letters <= 0)
+		return (0);
+	buf = malloc(letters);
 	if (fd < 0)
 		return (0);
 	total = read(fd, buf, letters);
