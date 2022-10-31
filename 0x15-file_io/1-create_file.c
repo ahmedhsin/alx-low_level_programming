86e8a46eb0e19092d92f8a31136d95834a3a3843
Yuu <ahmed.elia2003@gmail.com>
2022-10-31 15:09:24 +0200

    #3 task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd == -1)
 		fd = creat(filename, 0600);
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
-	if (write(fd, text_content, strlen(text_content)) < 0)
+	if (write(fd, text_content, strlen(text_content)) == -1)
 	{
 		close(fd);
 		return (-1);
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * append_text_to_file - check the code
+ * @filename: blablalba
+ * @text_content: blablalba
+ * Return: void
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (!text_content)
+	{
+		if (fd == -1)
+			return (-1);
+		else
+		{
+			close(fd);
+			return (1);
+		}
+	}
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT);
+	if (fd == -1)
+		return (-1);
+	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		return (-1);
+	}
+	return (1);
+
+}
