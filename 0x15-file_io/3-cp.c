bbc43bd7175ad52f3dad8ae145060dba84b131ec
Yuu <ahmed.elia2003@gmail.com>
2022-10-31 15:53:17 +0200

    #4.1 task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,8 +6,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * check - check
+ * @to:int
+ * Return: void;
+ */
+int check(int to)
+{
+	if (close(to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", to);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
  * main - check the code
- *
+ * @ac:bbb
+ * @av:ccc
  * Return: Always 0.
  */
 int main(int ac, char **av)
@@ -22,7 +38,7 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(98);
 	}
-	
+
 	from = open(av[1], O_RDONLY);
 	to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (from == -1)
@@ -45,16 +61,9 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	
-	if (close(from) == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	if (close(to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
+
+	check(from);
+	check(to);
+
 	return (1);
 }
