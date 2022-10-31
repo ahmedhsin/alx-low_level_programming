60160f1866d7d672039ec4d5057be86e1eacbf23
Yuu <ahmed.elia2003@gmail.com>
2022-10-31 13:51:43 +0200

    #1 task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * read_textfile - check the code
+ * @filename: blablalba
+ * @letters: blablalba
+ * Return: void
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	int *buf = malloc(letters);
+	int total = 0;
+
+	if (fd < 0)
+		return (0);
+	total = read(fd, buf, letters);
+	if (write(1, buf, letters) < 0 || total < 0)
+		return (0);
+	return (total);
+	
+}
