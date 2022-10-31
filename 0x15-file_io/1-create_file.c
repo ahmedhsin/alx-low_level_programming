03c53a524a41c2d5363cbdd1e819639f9aa50c48
Yuu <ahmed.elia2003@gmail.com>
2022-10-31 14:49:34 +0200

    #1,2 task - fix #10

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_RDWR | O_TRUNC);
 	if (fd == -1)
-		fd = creat(filename, 0770);
+		fd = creat(filename, 0600);
 	if (fd < 0)
 		return (-1);
 	if (write(fd, text_content, strlen(text_content)) == -1)
