ebd46cbd376633e47b2a13a81e8181a0f2cc34cd
Yuu <ahmed.elia2003@gmail.com>
2022-12-12 17:33:02 +0200

    ty again

--- 0x18-dynamic_libraries/ok.c
+++ 0x18-dynamic_libraries/ok.c
@@ -1,20 +1,120 @@
-int _putchar(char c){}
-int _islower(int c){}
-int _isalpha(int c){}
-int _abs(int n){}
-int _isupper(int c){}
-int _isdigit(int c){}
-int _strlen(char *s){}
-void _puts(char *s){}
-char *_strcpy(char *dest, char *src){}
-int _atoi(char *s){}
-char *_strcat(char *dest, char *src){}
-char *_strncat(char *dest, char *src, int n){}
-char *_strncpy(char *dest, char *src, int n){}
-int _strcmp(char *s1, char *s2){}
-char *_memset(char *s, char b, unsigned int n){}
-char *_memcpy(char *dest, char *src, unsigned int n){}
-char *_strchr(char *s, char c){}
-unsigned int _strspn(char *s, char *accept){}
-char *_strpbrk(char *s, char *accept){}
-char *_strstr(char *haystack, char *needle){}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _putchar(char c) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _islower(int c) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _isalpha(int c) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _abs(int n) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _isupper(int c) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _isdigit(int c) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _strlen(char *s) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+void _puts(char *s) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_strcpy(char *dest, char *src) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _atoi(char *s) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_strcat(char *dest, char *src) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_strncat(char *dest, char *src, int n) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_strncpy(char *dest, char *src, int n) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+int _strcmp(char *s1, char *s2) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_memset(char *s, char b, unsigned int n) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_memcpy(char *dest, char *src, unsigned int n) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_strchr(char *s, char c) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+unsigned int _strspn(char *s, char *accept) {}
+/**
+ * _putchar - lol
+ * @c: lol
+ * Return:int
+ */
+char *_strpbrk(char *s, char *accept) {}
+/**
+ * _putchar - lol
+ * @: lol
+ * Return:int
+ */
+char *_strstr(char *haystack, char *needle) {}
