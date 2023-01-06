c48433700c5488b2e31ac3d5762b1d08ba4f4d5d
Yuu <ahmed.elia2003@gmail.com>
2023-01-06 22:50:42 +0200

    try fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *head = malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
-	if (head == NULL)
+	if (head == NULL || size == 0)
 	{
 		return (NULL);
 	}
