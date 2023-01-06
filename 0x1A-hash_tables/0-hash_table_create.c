ce1a4bdddda613def15125c98c512eed5ad8ad87
Yuu <ahmed.elia2003@gmail.com>
2023-01-06 22:56:48 +0200

    tryfix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,17 +7,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *head = malloc(sizeof(hash_table_t));
+	hash_table_t *head;
 	unsigned long int i;
 
-	if (head == NULL || size == 0)
+	if (size <= 0)
 	{
 		return (NULL);
 	}
+	head = malloc(sizeof(hash_table_t));
 	head->size = size;
 	head->array = malloc(sizeof(hash_node_t) * size);
 	if (head->array == NULL)
 	{
+		free(head);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,10 @@
-#ifndef HASH
-#define HASH
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
+
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -13,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -28,8 +30,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -40,4 +42,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-#endif /*HASH*/
+hash_node_t *update_node(hash_node_t *node, const char *key, char *value);
+hash_node_t *new_node(hash_node_t *);
+
+#endif
