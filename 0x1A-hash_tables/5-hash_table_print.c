694620d8f632a2e457805c1af9907b7685fc6d2d
Yuu <ahmed.elia2003@gmail.com>
2023-01-07 01:43:17 +0200

    add print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hash
+ * @ht:head of hashtable
+ * Return:void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_table_t *head = (hash_table_t *)ht;
+	hash_node_t *tmp;
+	unsigned long int i, flag = -1;
+
+	if (head == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (i = 0; i < head->size; i++)
+	{
+		tmp = head->array[i];
+		while (tmp)
+		{
+			flag++;
+			if (flag != 0)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
