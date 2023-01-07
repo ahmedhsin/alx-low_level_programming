fe5eb136808eba90e7813a56a0c9a6dbdf8ff4aa
Yuu <ahmed.elia2003@gmail.com>
2023-01-07 02:22:27 +0200

    del

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,9 +10,9 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *tmp;
 	unsigned long int i = 0;
 
-	while (ht->array[i])
+	while (i < ht->size)
 	{
-		while (ht->array[i])
+		while (ht->array[i] != NULL)
 		{
 			tmp = ht->array[i]->next;
 			free(ht->array[i]->key);
