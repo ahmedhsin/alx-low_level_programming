fac66f420d8ee0d274994a0f057ac377f02e3ba9
Yuu <ahmed.elia2003@gmail.com>
2023-02-25 04:08:47 +0200

    fixing interview prob

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,26 +5,23 @@
 def get_val(grid, i, j):
     """return the number of faces not equal to one"""
     n = len(grid)
+    m = len(grid[0])
     value = 0
-    try:
-        if (grid[i-1][j] == 0):
-            value += 1
-    except Exception:
+    if (i-1 < 0):
         value += 1
-    try:
-        if (grid[i+1][j] == 0):
-            value += 1
-    except Exception:
+    elif grid[i-1][j] == 0:
         value += 1
-    try:
-        if (grid[i][j-1] == 0):
-            value += 1
-    except Exception:
+    if (i+1 >= n):
         value += 1
-    try:
-        if (grid[i][j+1] == 0):
-            value += 1
-    except Exception:
+    elif  grid[i+1][j] == 0:
+        value += 1
+    if (j-1 < 0):
+        value += 1
+    elif  grid[i][j-1] == 0:
+        value += 1
+    if (j+1 >= m):
+        value += 1
+    elif  grid[i][j+1] == 0:
         value += 1
     return value
 
@@ -33,7 +30,7 @@ def island_perimeter(grid):
     """Retun permiter func"""
     ans = 0
     for i in range(len(grid)):
-        for j in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 ans += get_val(grid, i, j)
     return ans
@@ -41,10 +38,6 @@ def island_perimeter(grid):
 
 if __name__ == "__main__":
     grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
+        [0, 1, 1, 0, 0, 0]
     ]
     print(island_perimeter(grid))
